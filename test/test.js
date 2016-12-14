f7e1aacb3441a9cf045245fdc32bc1e16add8b40
Joel <waylaidwanderer@live.com>
2016-12-14 04:19:38 -0800

    Add initial tests

---
+++ test/test.js
@@ -0,0 +1,123 @@
+"use strict";
+const assert = require('assert');
+const fs = require('fs');
+const RotatingProxyManager = require('../index');
+const RotatingProxy = RotatingProxyManager.RotatingProxy;
+
+describe('RotatingProxyManager', function() {
+    const self = this;
+    before(function() {
+        self.proxies = [];
+        let rotatingProxy = new RotatingProxy('127.0.0.1:80');
+        self.proxies.push(rotatingProxy);
+        rotatingProxy = new RotatingProxy('127.0.0.2:80');
+        rotatingProxy.setWaitInterval(5);
+        self.proxies.push(rotatingProxy);
+        rotatingProxy = new RotatingProxy('127.0.0.3:81');
+        rotatingProxy.setWaitInterval(2, 4);
+        self.proxies.push(rotatingProxy);
+    });
+    afterEach(function(done) {
+        self.proxyManager.database.db.close(() => {
+            done();
+        });
+    });
+    after(function(done) {
+        if (!self.proxyManager) return done();
+        fs.unlinkSync(__dirname + '/sqlite.db');
+        done();
+    });
+    describe('#getProxies()', function() {
+        it('length should equal number of proxies inserted', function(done) {
+            createProxyManager(self.proxies, true).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                self.proxyManager.getProxies((err, proxies) => {
+                    if (err) done(err);
+                    assert.equal(proxies.length, 3);
+                    done();
+                });
+            }).catch(err => {
+                done(err);
+            });
+        });
+    });
+    describe('#nextProxy()', function() {
+        it('should return correct proxy', function(done) {
+            createProxyManager(self.proxies, true).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.1:80');
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.2:80');
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.3:81');
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.1:80');
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+        it('should return correct proxy across multiple RotatingProxyManagers', function(done) {
+            //this.timeout(10000);
+            createProxyManager(self.proxies, true).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.1:80');
+                return createProxyManager(self.proxies, false, self.proxyManager);
+            }).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.2:80');
+                return createProxyManager(self.proxies, false, self.proxyManager);
+            }).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.3:81');
+                return createProxyManager(self.proxies, false, self.proxyManager);
+            }).then(proxyManager => {
+                self.proxyManager = proxyManager;
+                return nextProxy(self.proxyManager);
+            }).then(proxy => {
+                assert.equal(proxy.toString(), '127.0.0.1:80');
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+    });
+});
+
+function createProxyManager(proxies, newInstance = false, proxyManager = null) {
+    return new Promise(resolve => {
+        if (proxyManager) {
+            proxyManager.database.db.close(() => {
+                proxyManager = new RotatingProxyManager(proxies, __dirname, newInstance);
+                proxyManager.on('ready', function() {
+                    resolve(proxyManager);
+                });
+            });
+        } else {
+            proxyManager = new RotatingProxyManager(proxies, __dirname, newInstance);
+            proxyManager.on('ready', function() {
+                resolve(proxyManager);
+            });
+        }
+    });
+}
+
+function nextProxy(proxyManager) {
+    return new Promise((resolve, reject) => {
+        proxyManager.nextProxy((err, proxy) => {
+            if (err) return reject(err);
+            resolve(proxy);
+        });
+    });
+}
